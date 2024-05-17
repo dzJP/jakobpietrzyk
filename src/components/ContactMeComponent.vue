d71e8e4964cdd2bf473a216bff358cc8678d995f
dzjp <jakob_pietrzyk@outlook.com>
2024-05-17 21:53:11 +0200

    disable submit after click submit

--- src/components/ContactMeComponent.vue
+++ src/components/ContactMeComponent.vue
@@ -5,24 +5,24 @@
         </div>
         <form id="contactForm" class="contact-form" @submit.prevent="submitForm">
             <div class="input-box">
-                <input v-model="formData.name" type="text" name="fullName" id="fullName" placeholder="Name">
-                <input v-model="formData.email" type="email" name="emailAddress" id="emailAddress" placeholder="E-mail">
+                <input v-model="formData.name" :disabled="isSubmitting" type="text" name="fullName" id="fullName" placeholder="Name">
+                <input v-model="formData.email" :disabled="isSubmitting" type="email" name="emailAddress" id="emailAddress" placeholder="E-mail">
             </div>
             <div class="input-box">
-                <input v-model="formData.phoneNumber" type="tel" name="number" placeholder="Phonenumber" pattern="[+]?[0-9]+(\.[0-9]{1,2})?">
-                <input v-model="formData.subject" type="text" name="emailSubject" placeholder="Subject">
+                <input v-model="formData.phoneNumber" :disabled="isSubmitting" type="tel" name="number" placeholder="Phonenumber" pattern="[+]?[0-9]+(\.[0-9]{1,2})?">
+                <input v-model="formData.subject" :disabled="isSubmitting" type="text" name="emailSubject" placeholder="Subject">
             </div>
             <div class="input-box">
-                <textarea v-model="formData.message" name="emailMessage" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                <textarea v-model="formData.message" :disabled="isSubmitting" name="emailMessage" cols="30" rows="10" placeholder="Message"></textarea>
             </div>
-            <input type="submit" value="Submit" class="btn" name="send">
+            <input :disabled="isSubmitting" type="submit" :class="{'btn': true, 'btn-disabled': isSubmitting}" value="Submit" name="send">
         </form>
     </div>
 </template>
 
 <script>
-
 import axios from "axios";
+
 export default {
     data() {
         return {
@@ -31,33 +31,31 @@ export default {
                 email: '',
                 phoneNumber: '',
                 subject: '',
-                message:''
-            }
+                message: ''
+            },
+            isSubmitting: false
         };
     },
     methods: {
         async submitForm() {
+            this.isSubmitting = true;
             console.log('Form submitted:', this.formData);
             try {
                 const response = await axios.post(
-                    'https://portfolio-jakob.azurewebsites.net/api/v1/submitContactForm',
+                    'http://localhost:8080/api/v1/submitContactForm',
                     this.formData
                 );
                 console.log('Server response:', response.data);
+                // Handle successful submission logic here if needed
             } catch (error) {
                 console.error('Error submitting form:', error);
+                // Handle the error if needed
             }
-            this.formData = {
-                name: '',
-                email: '',
-                phoneNumber:'',
-                subject:'',
-                message: ''
-            };
         }
     }
 };
 </script>
+
 <style>
 .contact-container {
     min-height: 100vh;
@@ -78,7 +76,6 @@ export default {
     margin: 1rem auto;
     text-align: center;
     margin-bottom: 3rem;
-
 }
 
 .input-box {
@@ -137,32 +134,30 @@ export default {
     background-color: var(--blue-sky-hover);
 }
 
-@media (max-width: 360px)  {
+.btn-disabled {
+    background-color: var(--light-gray);
+    cursor: not-allowed;
+}
 
+@media (max-width: 360px) {
     .contact-container {
         padding: 10rem 5% 2rem;
-
     }
     .contact-form {
         max-width: 90%;
         margin-top: 30%;
     }
-
     .input-box {
         flex-direction: column;
         margin-bottom: 1rem;
-
     }
-
     .input-box input[type="text"],
     .input-box input[type="email"],
     .input-box input[type="tel"],
     .input-box textarea {
         margin: 5px 0;
         width: 100%;
-
     }
-
     .btn {
         padding: 0.5rem 1rem;
         font-size: 1rem;
@@ -178,8 +173,6 @@ export default {
         border: none;
         transition: background-color 0.5s ease;
         box-shadow: 0 0 1rem var(--blue-heaven-hover);
-
     }
-
 }
 </style>
